28839080dd7b73f6851735a1143e69af240fbbb1
Eduardo L Cotta <ducotta@gmail.com>
2013-10-23 23:26:42 -0200

    Corrigindo erros

--- test/AplicacaoFundoTeste.java
+++ test/AplicacaoFundoTeste.java
@@ -5,18 +5,20 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import ContaBancaria.AplicacaoFundo;
+
 public class AplicacaoFundoTeste {
 	
 	AplicacaoFundo umaAplicacao;
 
 	@Before
 	public void setUp() throws Exception {
-		umaAplicacao = new AplicacaoFundo(000-123-456-78, 1234);
+		umaAplicacao = new AplicacaoFundo("000-123-456-78", 1234);
 	}
 
 	@Test
 	public void testNumeroContaBancaria(){
-		assertEquals(000-123-456-78, umaAplicacao.getNumeroContaBancaria());
+		assertEquals("000-123-456-78", umaAplicacao.getNumeroContaBancaria());
 	}
 
 	@Test
@@ -25,27 +27,40 @@ public class AplicacaoFundoTeste {
 	}
 	
 	@Test
-	public void testDadosPessoais(){
+	public void testNome(){
 		umaAplicacao.setNome("umNome");
-		umaAplicacao.setEndereco("umEndereco");
-		umaAplicacao.setTelefone("umTelefone");
-		umaAplicacao.setCPF("umCPF");
 		assertEquals("umNome", umaAplicacao.getNome());
-		assertEquals("UmEndereco", umaAplicacao.getEndereco());
-		assertEquals("umTelefone", umaAplicacao.getTelefone());
-		assertEquals("umCPF", umaAplicacao.getCPF());
+	}
+
+	@Test
+	public void testEndereco(){
+		umaAplicacao.setEndereco("umEndereco");
+		assertEquals("umEndereco", umaAplicacao.getEndereco());
 	}
 	
-	@test
+	@Test
+	public void testOperacao(){
+		umaAplicacao.setOperacao("umaOperacao");
+		assertEquals("umaOperacao", umaAplicacao.getOperacao());
+	}
+	
+	@Test
+	public void testCpf(){
+		umaAplicacao.setCpf("umCPF");
+		assertEquals("umCPF", umaAplicacao.getCpf());
+	}
+	
+	@Test
 	public void testInvestimento(){
 		umaAplicacao.setInvestimento(8000.00);
 		assertEquals(8000.00, umaAplicacao.getInvestimento(), 0.001);
 	}
 	
 	@Test
-	public void testRendimento(){
-		umaAplicacao.setRendimento(80.00);
-		assertEquals(80.00, umaAplicacao.getRendimento(), 0.001);
+	public void testCalculaRendimento(){
+		umaAplicacao.setInvestimento(100000.0);
+		umaAplicacao.setPorcentagemRendimento(0.08);
+		assertEquals(8000, umaAplicacao.calculaRendimento(), 0.001);
 	}
 
 }
